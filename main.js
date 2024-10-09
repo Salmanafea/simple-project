1f362542171a0cb78aa610ba507127bde3d3b6d4
Salmanafea <92124163+Salmanafea@users.noreply.github.com>
2024-10-09 20:11:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let image = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones(phone){
+    image.src= phone;
+   
+}
+function colors(color){
+    container.style.background= color;
+
+
+}
